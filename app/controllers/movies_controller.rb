d1b95d390029e83ac347a06c030e094fed488902
Addrish Roy <ar4613@columbia.edu>
2023-10-02 02:31:24 +0000

    Part 1 changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,10 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show = params[:ratings].keys
+    @movies = Movie.with_ratings(@ratings_to_show)
+    #@ratings_to_show = params[:ratings].keys
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,20 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    ratings=[]
+    Movie.select(:rating).distinct.each do |m|
+      ratings << m.rating
+    end
+    ratings
+  end
+
+  def self.with_ratings(ratings_list)
+    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+    #  movies with those ratings
+    # if ratings_list is nil, retrieve ALL movies
+    if ratings_list==nil or ratings_list==[]
+      Movie.all
+    else
+      ratings_list=ratings_list.map(&:upcase)
+      Movie.where(rating:ratings_list)
+    end
 end
