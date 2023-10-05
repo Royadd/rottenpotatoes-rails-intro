3fdfcb43bb1aeabf5a92f29de8442a514b03f9c9
Addrish Roy <ar4613@columbia.edu>
2023-10-05 01:05:47 +0000

    autograder changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,27 +7,6 @@ class MoviesController < ApplicationController
   end
 
   def index
-    """sort_key = params[:sort_key] || session[:sort_key]
-    case sort
-    when 'title'
-      ordering,@title_header = {:title => :asc}, 'bg-warning hilite'
-    when 'release_date'
-      ordering,@date_header = {:release_date => :asc}, 'bg-warning hilite'
-    end
-    @all_ratings = Movie.all_ratings
-    @ratings_to_show = params[:ratings] || session[:ratings] || {}
-
-    if @ratings_to_show == {}
-      @ratings_to_show = Hash[@all_ratings.map {|rating| [rating, rating]}]
-    end
-
-    if params[:sort_key] != session[:sort_key] or params[:ratings] != session[:ratings]
-      session[:sort] = sort_key
-      session[:ratings] = @selected_ratings
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
-    end
-    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
-    """
     if not params.has_key?(:home)
       if params.has_key?(:check)
         session[:ratings]=params[:ratings]
@@ -35,18 +14,13 @@ class MoviesController < ApplicationController
     end
     if params[:sort_key]!=nil
       session[:sort_key]=params[:sort_key]
-      #redirect_to movies_path
-      #@css_class_2='.bg_warning'
     end
-    @sort_column=session[:sort_key]
     @all_ratings = Movie.all_ratings
     if session[:ratings]==nil
       @ratings_to_show=@all_ratings
     else
       @ratings_to_show = session[:ratings].keys
-      #redirect_to movies_path
     end
-    #@ratings_to_show = params[:ratings].keys
     if session[:sort_key]!=params[:sort_key] or session[:ratings]!=params[:ratings]
       redirect_to movies_path(sort_key: session[:sort_key], ratings: session[:ratings])
       return
